38986399c45612e4a500451c98e01ce05182680e
todddrinkwater <daniel-reason>
2017-05-10 15:21:54 +1200

    pass view animal by id test

--- tests/server.test.js
+++ tests/server.test.js
@@ -13,7 +13,17 @@ test('GET /', (t) => {
     .expect(200)
     .then((res) => {
       const $ = cheerio.load(res.text)
-      t.is($('li').first().text(), 'Ambitious Aardvark (aardvark@example.org)')
+      t.is($('li').first().text(), "Ambitious Aardvark (aardvark@example.org) ")
     })
 
+})
+
+test('GET /view_animal/:id', (t) => {
+  return request(t.context.app)
+  .get('/view_animal/99907')
+  .expect(200)
+  .then((res) => {
+    t.true(res.text.indexOf("Gila") > -1)
+    t.is(res.text.indexOf("Baboon"), -1)
+  })
 })
